9e2fe95bd75d0d421a68b340e43c1c10432bd64e
GFsugawara <sugawara@geekfeed.co.jp>
2024-11-26 13:52:58 +0900

    Slack連携の追加

--- lib/github-actions-slack-integration-stack.ts
+++ lib/github-actions-slack-integration-stack.ts
@@ -1,16 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
 
 export class GithubActionsSlackIntegrationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'GithubActionsSlackIntegrationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const s3Bucket = new s3.Bucket(this, 'S3Bucket', {
+      publicReadAccess: false,
+      versioned: true,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      bucketName: "github-actions-slack-integration-bucket-20241126"
+    });
   }
 }
